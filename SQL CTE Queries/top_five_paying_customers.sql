ef9448f0864aa1cf5ae1f607d62da1a47c535b4b
lopez-analytics <aliamislopez@gmail.com>
2024-07-23 15:17:51 -0600

    Add files via upload

---
+++ SQL CTE Queries/top_five_paying_customers.sql
@@ -0,0 +1,10 @@
+
+WITH TopFivePayingCustomers AS (
+    SELECT CustomerID, SUM(Amount) AS TotalPaid
+    FROM Payments
+    GROUP BY CustomerID
+    ORDER BY TotalPaid DESC
+    LIMIT 5
+)
+SELECT AVG(TotalPaid) AS AveragePaid
+FROM TopFivePayingCustomers;
